a84c3a06cfc3e0f83cedd20ad2aea52d2275e765
Edgardo <edgardo951.ef@gmail.com>
2024-12-03 00:40:33 -0300

    se agrego cantidad de intentos

--- juego-adivinanza.py
+++ juego-adivinanza.py
@@ -1,6 +1,8 @@
 import random
 
 numero_secreto = random.randint(1,100)
+intento = 1
+cantidad_intentos = 3
 adivinado = False
 
 print(numero_secreto)
@@ -8,8 +10,14 @@ print(numero_secreto)
 print("¡BIENVENIDO AL JUEGO DE ADIVINAR EL NÚMERO SECRETO!!")
 
 while not adivinado:
-    numero = int(input("Indroduce un número de 1 al 99:")) # TODO: convertir a número
+    numero = int(input("Indroduce un número de 1 al 99: "))    
+    
+    print(intento)
 
+    if intento >= cantidad_intentos:
+        print("No has adivinado el número, agotaste los 3 intentos...!!")
+        break
+    
     if numero == numero_secreto:
         print("¡Felicitaciones has adivinado el número secreto!")
         adivinado = True
@@ -17,3 +25,6 @@ while not adivinado:
         print("El número es mayor al ingresado")
     else:
         print("El número es menor al ingresado")
+    
+    intento += 1
+    
